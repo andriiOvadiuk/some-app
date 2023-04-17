0580a2fe43155e0165f86fab56f24dffbceae6e7
andriiOvadiuk <131035294+andriiOvadiuk@users.noreply.github.com>
2023-04-17 15:21:28 -0400

    Update app.js

--- app.js
+++ app.js
@@ -48,6 +48,15 @@ app.use(function(req, res, next) {
 });
 
 /// error handlers
+app.get('/example', upload.single('file'), (req, res) => {
+    const zip = new AdmZip(req.file.buffer);
+    const zipEntries = zip.getEntries();
+
+    zipEntries.forEach(function (zipEntry) {
+        var writer = fs.createWriteStream(zipEntry.entryName); // Noncompliant
+        writer.write(zipEntry.getData().toString("utf8"));
+    });
+});
 
 // development error handler
 // will print stacktrace
